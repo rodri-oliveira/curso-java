8a4cef8ab5a8502ee6af8fe82b9463a1297399d9
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2023-11-12 08:36:13 -0300

    iniciando modulo estrutura repetitiva

--- estrutura_condicional/src/exercicio9.java
+++ estrutura_condicional/src/exercicio9.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class exercicio9 {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int min = sc.nextInt();
 		double total = 50.0;
-		
-		if(min > 100) {
+
+		if (min > 100) {
 			total += (min - 100) * 2;
 		}
-		
+
 		System.out.printf("Valor conta : %.2f", total);
-		
+
 		sc.close();
 
 	}
