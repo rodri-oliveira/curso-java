c2b1de7acabc6c314016fb06b72e44b0f7004f11
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2023-11-24 11:26:10 -0300

    atualizando POO

--- search_binary/src/application/Main.java
+++ search_binary/src/application/Main.java
@@ -1,30 +1,29 @@
 package application;
 
 import java.util.Scanner;
-
 import entities.BuscaBinaria;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int[] array = {1,2,3,4,5};
-		BuscaBinaria busca = new BuscaBinaria(array);
-		
-		int alvo = sc.nextInt();
+    public static void main(String[] args) {
 
-		int resultado = busca.buscar(alvo);
-		
-		if(resultado != -1) {
-			System.out.println(alvo + " foi encontrado na posição "  + resultado);
-		}else {
-			System.out.println(alvo + " não encontrado. ");
-		}
-		
-		sc.close();
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int[] array = {1, 2, 3, 4, 5};
 
+        System.out.print("Digite o alvo: ");
+        int alvo = sc.nextInt();
+
+        BuscaBinaria busca = new BuscaBinaria();
+        int resultado = busca.buscar(array, alvo);
+
+        if (resultado != -1) {
+            System.out.println(alvo + " foi encontrado na posição " + resultado);
+        } else {
+            System.out.println(alvo + " não encontrado. ");
+        }
+
+        sc.close();
+    }
 }
+
